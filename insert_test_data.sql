317287dfaec669a24077ca10650e3929eca0498a
Sara <sshah004@campus.goldsmiths.ac.uk>
2025-11-20 20:33:04 +0000

    tester

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,5 +3,5 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
-INSERT INTO users (username, first, last, email, hashedPassword) VALUES ('gold', 'Gold', 'Smiths', 'goldsmiths@example.com',
-'$2b$10$CwSeIppZArYrusS4x2MA2.wN4IwEh1BcdSQGdXrJEnglishuLHxJHq');
+INSERT INTO users (username, first, last, email, hashedPassword) VALUES 
+('gold', 'Gold', 'Ring', 'gold@smiths.com','$2b$10$2uYUJBPiUhamxa8tUTdXPeXkzFInx8FQ7gq6w53VBCPqqlZ9G9lK6');
