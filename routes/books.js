c6f1de3d962cb3bd3d0684f27ca2932662f14eb6
Sara <sshah004@campus.goldsmiths.ac.uk>
2025-12-04 17:06:13 +0000

    books fixed

--- routes/books.js
+++ routes/books.js
@@ -2,10 +2,10 @@
 const express = require("express")
 const router = express.Router()
 
-// Get redirect function from app.locals
+// Access control – only for protected routes
 const redirectLogin = (req, res, next) => {
     if (!req.session || !req.session.userId) {
-        return res.redirect('../users/login'); // Correct redirect path
+        return res.redirect('../users/login');
     }
     next();
 };
@@ -20,43 +20,37 @@ router.get('/search-result', function (req, res, next) {
     res.send("You searched for: " + req.query.keyword)
 });
 
-// Book list - login required
-router.get('/list', redirectLogin, function(req, res, next) {
+// Book list - PUBLIC (no login required)
+router.get('/list', function(req, res, next) {
     let sqlquery = "SELECT * FROM books";
     db.query(sqlquery, (err, result) => {
-        if (err) { next(err) }
-        else {
-            res.render("list.ejs", { availableBooks: result });
-        }
+        if (err) next(err)
+        else res.render("list.ejs", { availableBooks: result });
     });
 });
 
-// Add book form - login required
+// Bargains - PUBLIC (no login required)
+router.get('/bargainbooks', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+    db.query(sqlquery, (err, result) => {
+        if (err) next(err)
+        else res.render("bargainbooks.ejs", { cheapBooks: result });
+    });
+});
+
+// Add book form - LOGIN REQUIRED
 router.get('/addbook', redirectLogin, function (req, res, next) {
     res.render('addbook.ejs');
 });
 
-// Insert book - login required
+// Insert book - LOGIN REQUIRED
 router.post('/bookadded', redirectLogin, function (req, res, next) {
     let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
     let newrecord = [req.body.name, req.body.price];
 
     db.query(sqlquery, newrecord, (err, result) => {
-        if (err) { next(err) }
-        else {
-            res.send('This book is added to database, name: ' + req.body.name + ' price ' + req.body.price);
-        }
-    });
-});
-
-// Bargains - login required
-router.get('/bargainbooks', redirectLogin, function(req, res, next) {
-    let sqlquery = "SELECT * FROM books WHERE price < 20";
-    db.query(sqlquery, (err, result) => {
-        if (err) { next(err) }
-        else {
-            res.render("bargainbooks.ejs", { cheapBooks: result });
-        }
+        if (err) next(err)
+        else res.send('This book is added to database, name: ' + req.body.name + ' price ' + req.body.price);
     });
 });
 
